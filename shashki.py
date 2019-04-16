7da8651d702454a0901cdb34a485134efcef4cf5
Majtreja <d.bielov@gmail.com>
2019-04-16 16:43:53 +0300

    16.04.v3 lessons of coursera

--- shashki.py
+++ shashki.py
@@ -1,15 +1,6 @@
-v1 = int(input())
-g1 = int(input())
-v2 = int(input())
-g2 = int(input())
-a1 = min(g1, g2, v1, v2)
-a2 = max(g1, g2, v1, v2)
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
 
-if (g2 > g1 and
-        g2 - g1 >= abs(v2 - v1) and
-        ((v1 % 2 == v2 % 2 and (g2 - g1) % 2 == 0) or (v1 % 2 != v2 % 2 and (g2 - g1) % 2 != 0)) and
-        a1 > 0 and
-        a2 < 9):
-    print('YES')
-else:
-    print('NO')
+print("YES") if abs(x1 - x2) % 2 == abs(y1-y2) % 2 else print("NO")
